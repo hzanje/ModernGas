278d612de411bd5d95944bded873e4d7ae7d996f
HZanje <analyticshemz@gmail.com>
2021-07-17 17:25:03 +0530

    Update InventoryRepo.java

--- src/main/java/com/moderngas/repository/InventoryRepo.java
+++ src/main/java/com/moderngas/repository/InventoryRepo.java
@@ -5,6 +5,7 @@ import com.moderngas.enums.CylinderStatus;
 import com.moderngas.jpaentity.CylinderEntity;
 import com.moderngas.pojo.admin.InventoryCylinderDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -19,11 +20,13 @@ public interface InventoryRepo extends JpaRepository<CylinderEntity, Long> {
     @Query("SELECT code FROM CylinderEntity WHERE cylinderStatus =:status")
     List<String> getAvailableCylinder(@Param("status") CylinderStatus status);
 
+    @Modifying
     @Query("UPDATE CylinderEntity SET cylinderStatus =:status, userId=:userId WHERE code IN (:codeList) ")
     void updateCylinderToAssigned(@Param("userId") Long userId,
                                   @Param("codeList") List<String> codeList,
                                   @Param("status") CylinderStatus status);
 
+    @Modifying
     @Query("UPDATE CylinderEntity SET cylinderStatus =:status, userId = NULL WHERE code IN (:codeList) AND userId=:userId")
     void updateCylinderToEmpty(@Param("userId") Long userId,
                                @Param("codeList") List<String> codeList,
