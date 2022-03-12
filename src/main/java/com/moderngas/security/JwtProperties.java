07754cb5e58a00455bbc8a66f6c9e6cd2370e9df
HZanje <analyticshemz@gmail.com>
2022-03-13 05:04:02 +0530

    Update JwtProperties.java

--- src/main/java/com/moderngas/security/JwtProperties.java
+++ src/main/java/com/moderngas/security/JwtProperties.java
@@ -3,8 +3,8 @@ package com.moderngas.security;
 public class JwtProperties {
 
     public static final String SECRET = "@bhishek#567B";
-
-    public static final int EXPIRATION_TIME = 864000000; /*420000*/ /*864000000*/
+    
+    public static final int EXPIRATION_TIME = 70000; /*70000*/ /*864000000*/
 
     public static final String TOKEN_PREFIX = "Bearer ";
 
